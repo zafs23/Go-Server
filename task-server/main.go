96ac45c8df044f076201b922849dba40aab61387
zafs23 <sajiazafreen@u.boisestate.edu>
2024-09-30 15:33:59 -0700

    Add the server connection

--- task-server/main.go
+++ task-server/main.go
@@ -1,8 +1,23 @@
 package main
 
-import "log"
+import (
+	"go-task-server/task-server/server"
+	"log"
+	"sync"
+)
+
+// declare the ports you want to listen
+var ports = []int{3000} // add other ports here
 
 func main() {
-	
-	log.Println("initial code")
+	var wg sync.WaitGroup
+
+	for _, port := range ports {
+		wg.Add(1)
+		//fmt.Printf("%v", port)
+		go server.StartListener(&wg, port)
+	}
+
+	wg.Wait()
+	log.Println("All server tasks are completed")
 }
--- task-server/server/server.go
+++ task-server/server/server.go
@@ -1,1 +1,44 @@
 package server
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"sync"
+)
+
+func StartListener(wg *sync.WaitGroup, port int) {
+	defer wg.Done() // signal the task is done to go routine
+
+	ep := fmt.Sprintf("127.0.0.1:%d", port)
+
+	listener, err := net.Listen("tcp", ep)
+	if err != nil {
+		log.Fatalf("Failed to start listening on port %d: %v", port, err)
+	}
+
+	defer listener.Close()
+
+	log.Printf("Started listening on %s", ep)
+
+	// accept connections in a loop
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("Cannot accept connection on port %d: %v", port, err)
+			continue
+		}
+
+		// handle the connection in a concurrently
+		go HandleConnection(conn)
+
+	}
+}
+
+func HandleConnection(conn net.Conn) {
+	defer conn.Close()
+	log.Printf("Started executing tasks from %v", conn.RemoteAddr())
+
+	
+
+}
